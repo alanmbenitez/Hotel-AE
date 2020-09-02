cf08cdaab5892211fa0ba420609d651c807dcbaa
Herrnnann <boerohernann@gmail.com>
2020-09-02 23:52:43 +0200

    Componetice los elementos de la home

---
+++ Hotel-AE/resources/views/components/home/blog.blade.php
@@ -0,0 +1,107 @@
+<section id="blog">
+    <div class="container">
+        <div class="main_title mt_wave a_center">
+            <h2>LATEST NEWS & EVENTS</h2>
+        </div>
+        <p class="main_description a_center">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit.</p>
+
+        <div class="row">
+            <!-- ITEM -->
+            <div class="col-md-6 col-sm-6">
+                <article class="blog_item">
+                    <div class="row">
+                        <div class="col-lg-5 col-md-12">
+                            <figure>
+                                <a href="blog-post.html" class="hover_effect h_blue h_link"><img src="images/blog/latest_news1.jpg" class="img-responsive" alt="Image"></a>
+                            </figure>
+                        </div>
+                        <div class="col-lg-7 col-md-12">
+                            <div class="main">
+                                <h3><a href="blog-post.html">Live your myth in Greece</a></h3>
+                                <p>Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore...</p>
+                                <a href="blog-post.html" class="button btn_xs btn_blue">Read More</a>
+                                <div class="info">
+                                    <i class="fa fa-user" aria-hidden="true"></i><a href="#">JOHN DOE</a>
+                                    <i class="fa fa-calendar" aria-hidden="true"></i><a href="#">AUGUST 13, 2015</a>
+                                    <i class="fa fa-comment-o" aria-hidden="true"></i><a href="#">4 COMMENTS</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </article>
+            </div>
+            <!-- ITEM -->
+            <div class="col-md-6 col-sm-6">
+                <article class="blog_item">
+                    <div class="row">
+                        <div class="col-lg-5 col-md-12">
+                            <figure>
+                                <a href="blog-post.html" class="hover_effect h_blue h_link"><img src="images/blog/latest_news2.jpg" class="img-responsive" alt="Image"></a>
+                            </figure>
+                        </div>
+                        <div class="col-lg-7 col-md-12">
+                            <div class="main">
+                                <h3><a href="blog-post.html">Hotel Zante in pictures</a></h3>
+                                <p>Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore...</p>
+                                <a href="blog-post.html" class="button btn_xs btn_blue">Read More</a>
+                                <div class="info">
+                                    <i class="fa fa-user" aria-hidden="true"></i><a href="#">JOHN DOE</a>
+                                    <i class="fa fa-calendar" aria-hidden="true"></i><a href="#">AUGUST 16, 2015</a>
+                                    <i class="fa fa-comment-o" aria-hidden="true"></i><a href="#">6 COMMENTS</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </article>
+            </div>
+            <!-- ITEM -->
+            <div class="col-md-6 col-sm-6">
+                <article class="blog_item">
+                    <div class="row">
+                        <div class="col-lg-5 col-md-12">
+                            <figure>
+                                <a href="blog-post.html" class="hover_effect h_blue h_link"><img src="images/blog/latest_news3.jpg" class="img-responsive" alt="Image"></a>
+                            </figure>
+                        </div>
+                        <div class="col-lg-7 col-md-12">
+                            <div class="main">
+                                <h3><a href="blog-post.html">Hotel Zante family party</a></h3>
+                                <p>Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore...</p>
+                                <a href="blog-post.html" class="button btn_xs btn_blue">Read More</a>
+                                <div class="info">
+                                    <i class="fa fa-user" aria-hidden="true"></i><a href="#">JOHN DOE</a>
+                                    <i class="fa fa-calendar" aria-hidden="true"></i><a href="#">SEPTEMBER 25, 2015</a>
+                                    <i class="fa fa-comment-o" aria-hidden="true"></i><a href="#">3 COMMENTS</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </article>
+            </div>
+            <!-- ITEM -->
+            <div class="col-md-6 col-sm-6">
+                <article class="blog_item">
+                    <div class="row">
+                        <div class="col-lg-5 col-md-12">
+                            <figure>
+                                <a href="blog-post.html" class="hover_effect h_blue h_link"><img src="images/blog/latest_news4.jpg" class="img-responsive" alt="Image"></a>
+                            </figure>
+                        </div>
+                        <div class="col-lg-7 col-md-12">
+                            <div class="main">
+                                <h3><a href="blog-post.html">Hotel Zante weddings</a></h3>
+                                <p>Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore...</p>
+                                <a href="blog-post.html" class="button btn_xs btn_blue">Read More</a>
+                                <div class="info">
+                                    <i class="fa fa-user" aria-hidden="true"></i><a href="#">JOHN DOE</a>
+                                    <i class="fa fa-calendar" aria-hidden="true"></i><a href="#">OCTOBER 20, 2015</a>
+                                    <i class="fa fa-comment-o" aria-hidden="true"></i><a href="#">0 COMMENTS</a>
+                                </div>
+                            </div>
+                        </div>
+                        </div>
+                </article>
+            </div>
+        </div>
+        </div>
+</section>
