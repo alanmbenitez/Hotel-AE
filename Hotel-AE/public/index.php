0452c17cd8066c49d9eab048d518463b5c1ea1eb
Alan Benitez <alan@digitalhouse.com>
2020-09-12 00:55:30 -0300

    prueba

--- Hotel-AE/public/index.php
+++ Hotel-AE/public/index.php
@@ -9,6 +9,14 @@
 
 define('LARAVEL_START', microtime(true));
 
+if((isset($_SERVER["HTTP_X_FORWARDED_PROTO"]) && $_SERVER["HTTP_X_FORWARDED_PROTO"] === 'http')){
+    // redireccionar a https
+    $redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    header('HTTP/1.1 301 Moved Permanently');
+    header('Location: ' . $redirect);
+    exit();
+}
+
 /*
 |--------------------------------------------------------------------------
 | Register The Auto Loader
--- Hotel-AE/resources/views/components/general/template_email.blade.php
+++ Hotel-AE/resources/views/components/general/template_email.blade.php
@@ -416,9 +416,7 @@
                                                                             
                                                                                 <svg version="1.0" xmlns="http://www.w3.org/2000/svg" height="90" viewBox="0 0 1280.000000 1927.000000"
                                                                                 preserveAspectRatio="xMidYMid meet">
-                                                                               <metadata>
-                                                                               Created by potrace 1.16, written by Peter Selinger 2001-2019
-                                                                               </metadata>
+                                                                              
                                                                                <g transform="translate(0.000000,1927.000000) scale(0.100000,-0.100000)"
                                                                                fill="#ED325C" stroke="none">
                                                                                <path fill="#ED325C"  d="M3900 19263 c-103 -13 -290 -57 -393 -93 -484 -167 -867 -501 -1094
