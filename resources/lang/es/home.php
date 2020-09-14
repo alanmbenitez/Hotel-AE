5622344228fd9462780a37fe57fb0169dfb1dfd2
AgustinAzpeitia <ezequielazp@gmail.com>
2020-09-14 00:22:50 -0300

    internacionalizacion de services

--- resources/lang/es/home.php
+++ resources/lang/es/home.php
@@ -11,5 +11,42 @@ return [
     */
     // EJ
     'title' => 'Bienvenidos al Hotel Aires Express',
-    
+    'title_services' => 'NUESTROS SERVICIOS',
+    'subtitle_services' => 'Ofrecemos los siguientes servicios para que puedas aprovechar al máximo tu estadía en el hotel.',
+
+    'img_services' => [
+        'img1' => [
+            'src' => asset('images/home/breakfast.webp'),
+            'alt' => 'breakfast'
+        ],
+        'img2' => [
+            'src' => asset('images/home/aire.webp'),
+            'alt' => 'aire'
+        ],
+        'img3' => [
+            'src' => asset('images/home/conserje.webp'),
+            'alt' => 'conserje'
+        ]
+        ],
+
+    'info_services' => [
+        'info1' => [
+            'src' => asset('images/icons/coffee.svg'),
+            'alt' => 'icon1',
+            'title' => 'Desayuno continental libre',
+            'description' => 'Café con leche, medialunas, tostadas',
+        ],
+        'info2' => [
+            'src' => asset('images/icons/air.svg'),
+            'alt' => 'icon2',
+            'title' => 'Aire acondicionado frío/calor',
+            'description' => 'Cada uno con regulación individual',
+        ],
+        'info3' => [
+            'src' => asset('images/icons/conserje.svg'),
+            'alt' => 'icon3',
+            'title' => 'Conserjería 24 horas',
+            'description' => 'Atención personalizada cuando necesites',
+        ],
+    ]
 ];
