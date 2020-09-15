c542de968539d5e0767cdf725d4dc16654e57e9d
Alan Benitez <alan@digitalhouse.com>
2020-09-14 23:54:16 -0300

    ajustes de estilos

---
+++ resources/views/components/icons/coffee_home.blade.php
@@ -0,0 +1,6 @@
+
+<svg width="40px" 
+padding="5px" 
+
+xmlns="http://www.w3.org/2000/svg" 
+viewBox="0 0 350 350" style=" margin-left: 28px">><defs><style>.cls-1,.cls-2,.cls-3{fill:none;}.cls-2,.cls-3{stroke:#ed325c;stroke-linecap:round;stroke-miterlimit:10;}.cls-2{stroke-width:8px;}.cls-3{stroke-width:6px;}</style></defs><title>Desayuno_1_2</title><g id="Layer_2" data-name="Layer 2"><g id="Desayuno"><g id="Desayuno_1_2"><circle class="cls-1" cx="175" cy="175" r="175"/><path class="cls-2" d="M199.61,249.22h-81a13.1,13.1,0,0,1-13.1-13.1V104.32a1.64,1.64,0,0,1,1.64-1.64h104a1.64,1.64,0,0,1,1.64,1.64v131.8A13.1,13.1,0,0,1,199.61,249.22Z"/><path class="cls-2" d="M212.71,194.29h24.72a13.1,13.1,0,0,0,13.1-13.1v-52.8a1.63,1.63,0,0,0-1.64-1.64H212.71"/><line class="cls-3" x1="174.19" y1="138.76" x2="174.19" y2="107.04"/><rect class="cls-3" x="160.82" y="140.05" width="27.63" height="38.68" rx="1.64" ry="1.64"/></g></g></g></svg>
---
+++ resources/views/components/icons/wifi_home.blade.php
@@ -0,0 +1,16 @@
+<svg width="40px" padding="5px" style=" margin-left: 28px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 350 350">
+    <defs>
+        <style>.cls-1,.cls-3{fill:none;}.cls-2{fill:#ed325c;stroke-width:17px!important;}.cls-3{stroke:#ed325c;stroke-linecap:round;stroke-miterlimit:10;stroke-width:20px !important;}</style>
+    </defs>
+    <g id="Layer_2" data-name="Layer 2">
+        <g id="Wifi">
+            <g id="Wifi_1">
+                {{-- <circle class="cls-1" cx="175" cy="175" r="175"/> --}}
+                <circle class="cls-2" cx="175" cy="231.58" r="13.51"/>
+                <path class="cls-3" d="M204.23,202.36a41.33,41.33,0,0,0-58.38-.08"/>
+                <path class="cls-3" d="M229.55,174.88a78.68,78.68,0,0,0-109.1,0"/>
+                <path class="cls-3" d="M255.28,147.11a116.56,116.56,0,0,0-160.57,0"/>
+            </g>
+        </g>
+    </g>
+</svg>
